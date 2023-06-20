4f5f55e1fae9007c22782e000efb358adea564c1
Manish <130898354+ManishDeve@users.noreply.github.com>
2023-06-20 23:53:59 +0530

    add seller menu and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller/seller-home/seller-home.component';
 import { SellerAuthGuard } from 'src/assets/auth-guard/seller-auth.guard';
+import { SellerAddProductComponent } from './seller/seller-add-product/seller-add-product.component';
 
 const routes: Routes = [ 
   { path:"",component:HomeComponent},
   {path:"seller-auth",component:SellerAuthComponent },
-  {path:"seller-home",component:SellerHomeComponent , canActivate:[SellerAuthGuard] }
+  {path:"seller-home",component:SellerHomeComponent , canActivate:[SellerAuthGuard] },
+  {path:"seller-add-product",component:SellerAddProductComponent , canActivate:[SellerAuthGuard] }
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  menuType:string='default';
+  sellerName:string='';
+  constructor(private _router:Router){
+
+  }
+  ngOnInit(): void {
+    this._router.events.subscribe((val:any) =>{
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          this.menuType="seller";
+          if(localStorage.getItem('seller')){
+            let sellerStore=localStorage.getItem('seller');
+            let sellerData=sellerStore && JSON.parse(sellerStore)[0];
+            // console.warn(sellerData.name);
+            this.sellerName=sellerData.name;
+          }
+        } else {
+          this.menuType="default";
+        }
+      }
+    })
+  }
+
+
+  logout(){
+    localStorage.removeItem('seller')
+    this._router.navigate(['/']);
+  }
 }
